483a31f9038306b628a6db3e3bea20876c34a118
SamanthaDiazWebDev <samantha.diaz1019@gmail.com>
2023-07-06 16:55:37 -0400

    Final Project React

--- src/App.js
+++ src/App.js
@@ -5,19 +5,16 @@ import style from './App.module.css';
 function App() {
   const tableName = process.env.REACT_APP_TABLE_NAME;
   return (
-    <div className={style.App}>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<TodoContainer tableName={tableName}/>} />
-          <Route exact path="/new" element={<h1>New Todo List</h1>} />
-        </Routes>
-        <br></br>
-        <span className={style.Spacing}></span>
-        <footer>
-          <h3>Samantha Diaz, 2023</h3>
-        </footer>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<TodoContainer tableName={tableName}/>} />
+        <Route exact path="/new" element={<h1>New Todo List</h1>} />
+      </Routes>
+      <br></br>
+      <footer>
+        <h3>Samantha Diaz, 2023</h3>
+      </footer>
+    </BrowserRouter>
   );
 };
 
